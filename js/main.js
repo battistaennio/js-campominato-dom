84f54fc07e2b7fc2713a78ede524ce49596a9c6b
battistaennio <ennio.battista99@gmail.com>
2024-06-14 16:38:25 +0200

    feat: add defeat/win output in page

--- js/main.js
+++ js/main.js
@@ -5,13 +5,25 @@
 
 
 //Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
 //In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
 //Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
 //La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 //seleziono il bottone gioca
 const playButton = document.querySelector("button");
+
+//seleziono l'output per il risultato di sconfitta in pagina
+const resultDefeat = document.getElementById("result-defeat-output");
+
+//seleziono l'output per il risultato di vittoria in pagina
+const resultWin = document.getElementById("result-win-output");
+
+//seleziono l'output per il punteggio
+const showPoints = document.getElementById("points");
 
 //creo evento su click
 playButton.addEventListener("click",
@@ -51,23 +63,29 @@ playButton.addEventListener("click",
             //aggioungo i div.box in div.container
             addContainer.append(addBox);
 
-
-            //creo evento cambio colore al click di div.box
+            //creo evento al click di div.box
             addBox.addEventListener("click",
                 function () {
 
                     //se il numero bomba e il numero div.box combacia, aggancio la bomba
                     if(bmbArray.includes(i)){
-                        addBox.classList.add("bomb")
+                        addBox.classList.add("bomb");
+                        //mostro il messaggio "hai perso"
+                        resultDefeat.classList.remove("vanish");
+                        showPoints.innerText = points;
 
                     } else {
                         //altrimenti aggiungo la classe clicked
                         addBox.classList.add("clicked");
                         //aggiungo un punto
                         points += 1;
+                        showPoints.innerText = points;
                     }
-                    console.log(points);
-                    
+
+                    //condizione di vincita
+                    if(points >= 86) {
+                        resultWin.classList.remove("vanish");
+                    }                    
 
                 }
                 
